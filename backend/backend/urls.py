4930eb83d04c90dbdfb777f7da47ce41ed3471c3
B.Nandhakumar <nandhakumarid023@gmail.com>
2025-08-06 14:21:50 +0530

    urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
-    path('api/', include('accounts.urls')), 
+    path('api/', include('accounts.urls')),
+    path('', lambda request: HttpResponse("Hello from Render!")),
 ]
 
 if settings.DEBUG:
